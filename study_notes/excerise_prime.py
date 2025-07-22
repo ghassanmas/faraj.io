d126dccdb3cbb2d723f376a8765bc4be1309a99f
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2025-07-22 11:43:28 +0200

    sqush: not ur biz, are you paying me

--- study_notes/excerise_prime.py
+++ study_notes/excerise_prime.py
@@ -11,17 +11,34 @@ from is_prime import is_prime_dump
 
 # let's do for 
 
-def draw_for_x_y(x=2, y=3, s=10):
+#Todo:
+# the calcualted range shuld be used for drawings, instead of hard coding
+# Refactor code
+
+def draw_for_x_y(x=2, y=3, s=10, prime_only=True):
     """
+    X: is the value to multilpy with 
+    Y: is the value to add with
+    S: is the range of n where n is: (0, 1, 2 ... S-1)
+    prime_only: when true draw calculate value of n of S that
+    is prime
     Given X,Y calcualte for range of S
+    Note: when prime_only is true, it relfect the range, not the calculated
+    value
     if it's prime X
     if it's not write it
     """
+    # Calcualte range of S ...S-1, if prime only is True, filter the range for 
+    #  prime numbers
     result = []
-    max_digit_calc = len(str(((s-1) * 2) + 3))
+    range_s = [i for i in range(0,s) if (is_prime_dump(i) or not prime_only) ]
+    max_digit_calc = len(str(((range_s[-1]) * x) + y))
     #max_digit_range = len(str(s-1))
     pad = 10 - max_digit_calc
     for i in range(0, s):
+        if prime_only:
+            if is_prime_dump(i):
+                continue
         calc = (i * x) + y
         if (is_prime_dump(calc)):
             result.append("  " + f'\033[92m{calc}'.ljust(pad, ' ').rjust(2, ' '))
@@ -36,7 +53,7 @@ def draw_for_x(x=2, s=10):
     for y in range(0, 10):
         result_2d.append(draw_for_x_y(x, y, s))
     # draw
-    str_to_print = f'\033[95m /  | '
+    str_to_print = f'\033[95m (x,y)\\n| '
     for y in range(0, 10):
         str_to_print += f'\033[95m {y}  | '
     print(str_to_print)
@@ -44,8 +61,9 @@ def draw_for_x(x=2, s=10):
     print(break_line)
 
     for y in range(0, 10):
-        str_to_print = f'\033[95m{x}, {y}|'
-        for y1 in range(0, 10):
+        str_to_print = "  " + f'\033[95m{x},{y}   |'
+        range_ = len(result_2d[y])
+        for y1 in range(0, range_):
             pad = 10 - len(str(result_2d[y][y1]))
             str_to_print+= f"{str(result_2d[y][y1]).ljust(pad, ' ')}"+"\033[95m|"
         print(str_to_print)
