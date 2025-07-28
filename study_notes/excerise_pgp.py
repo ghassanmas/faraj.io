c9a9dc0d1c6518004bca63bb81789ac90778d5da
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2025-07-28 19:06:57 +0200

    next: pgp exec
     as it's clear public key cannot be used for message transmision
     at least that what resources are saying(some funded by NSF) like
     34 ref in last commit paper.
    
    That being said, is pgp random session to create key sufficent
    and what is pgp session in anyway.
    
    Beside that open questoin regarding diffie-himman (discrete logs)
    and RSA (integer factorization) aren't yet answered.
    
    Fermat theroerm, jacobs sum, seems to be used in algorthim.
    
    Also one simple questoin how long does it take to do a dump
    check if a number is prime which is at least 100(digit).
    
    Thus moving one the tasks are:
     1: play with pgp and study it
     2: how long does it take 100 digit random number
       - 200
       - 300
      - Lastly one hint (1/115) [from ref of prev commit], that a
     a number us prime (then just add +2 of course it's odd).
     thus time to find a number is really prime: **whatever time
     took for 2.1 or 2.2  * 115 for worst case scinario
    3: still non of ur biz.
    
     relate #30 (The heck this taken forever)

---
+++ study_notes/excerise_pgp.py
@@ -0,0 +1,105 @@
+"""
+Given the following m,c messages pair which encrypted using (same public key)
+m1=hello t1=~28.7.25 ~~18:45 EST
+-----BEGIN PGP MESSAGE-----
+
+hQIMA+cxB/nvl1l8AQ/8CSa56gapSlUEkQyZNQFx0m6z9chPh9vt/zNVodmcXiL7
+F9kAUC3ZVjrurFbHXxWzA5kv5YNdfYch6s6wq6fAb8LrziFfHOzROSo6bG9crlK5
+yN/2bHczGu04x62qXtw58cojYmfWAkwrq/boHv0PrsZIfkAPdJO+03jleqFPHBv9
+mfFJaVIvhTozEPRvPhgRAON3ZM7o18iScyYeVXE0guNsH4OtDlX03nkInMarXWbm
+I2BjTKdgchnz9FmyRQ6fb+GWqVvDWoot2cAbfezXjB8Te1c4yJd9tSk2uD8sJEAY
+S1x1Ji4BVNr+8M9PCYUD0AQqGqpLBrjGgs7iCEiuibqYijRRbFuPZyTjXprxXrvs
+7acNpqfL/l7qcU+oUiY9esbFOd0CJskw58zsCmN3xbJCDRQXM+drwo5qbXxVU32J
+gk31T/WFJ7mIMqhist5BvWWW73W0AdZcwdEDVzdNjqqjbJrOv3kVhOSxrLKzUAc2
+0LjmZwW707TfWvVUkkHRU5as5BaU+yPDgEGkCNWQs9RG4ABgXv0e3gXIgbxNjngQ
+AwJNARjj3Xurel5FjFUFq7FhTK/UnEfdF+GcDemB5ELth2ZlACmC4uB6pJDN8BKZ
+YE28vPiRiNGlHLxfQGvOcVGAuXOG1xF5bCPaQzsSWz9PSDJ5vnqPYmArnNlPJZ3S
+QAH75v+6RUkvsPvhQ1yVTylAeTpLVtFPa3zpez5tkdlZcr6gEZ72lZC4qiwhDTxQ
+hmuvFc9ruoe2iTwkgkOU9gI=
+=ti1j
+-----END PGP MESSAGE-----
+m2=m1 t2 > t1
+-----BEGIN PGP MESSAGE-----
+
+hQIMA+cxB/nvl1l8AQ/9EA8qQLuRLzJJ3XN/l85loxeMoRxjWtAaVawU/t3FFH5B
+He1RIdbM3KcLLGRy63Rv1E88JZzZfLe3DpJDEL3w/5agEna6qdlYL8EYzLZC4XPK
+VQAZHwZ+teMwN82KNwNZaZYq8nL+s0/B0/y28ryYsW0lFUjqAru0+IefnmVzR7Jh
+ThSqOdW6lJpMH0NGMpDnlPIHb7M3PBgbxVtX+mrjLGZbyjn4upYgxVFREjRjg0s9
+oF2BBh2aGoWFJ4c8MkNC7294xmRiSqRjmtdGoTTO1km8MQo0hlMft6O5Ppa8EhOT
+guuRapiiu+mo/lvqbVlxyEUlCi95Tl0Gid3i6l86DCBAooiZQ+ZcWmsyyJ5nt4FU
+Wi4kKDoDLk5WW0ex7Gso1b9SRDxNwIlOGUkBvmmWjozDIogwhvtyyZMl4Yxc1k2c
+4heNx7rQqRrTMuDciFKl1s+ZqwzxGZUlP+H5+PBqOkjBF5epN/Ae+GRvdHS/w59S
+Wp+/fCntj9lXYlDiwTWPC2ML+zH9eAaGbh6Bwq06RW9iAvYs2t/mGpwOE5S6Smz0
+vVnswouZZZPM/dqgxuENOfAMbg9m04EuP+SfxkQlo2EtGDEmBJzeV6Bis3ncioGQ
+nClOZ2v3IzVhlwq9k2QxsLD/hU1narqptMntQBSK6I8ueDPu9s8a0zrASI1MhszS
+QAHD+6EYOwiraCsLS7PVw4wVKM1PhwRUGUh6l1WZg/J/TLkjtK6U20GZz/Kc7nXu
+b7fIQq38tj/ld5xiWNyugf8=
+=ywdq
+-----END PGP MESSAGE-----
+
+m3=what t3>t2
+-----BEGIN PGP MESSAGE-----
+
+hQIMA+cxB/nvl1l8AQ/8CPu+V2fj7yR3iRFsgfk57zkY3PIgq8WeyzkHslEtp35j
+M8iRDhM36RaOOgihcwy9wMxfke/rsCZKeIWWrvFAANl9j+dfQDW0mjJOLtH8U2P+
+07AGhlgCdTME2ad/EtIhhNpq7rGJFSC3gSE9AXxWWZZNascTlWEJurjWtiULpUar
+G/FjMN9ElSHk5aXMHnYy2TkejFecBfA5w9acTG8cnBWlz7Mu1O9ZRh8AJ4baoUTW
+3l74cwlNt+KE93rVRYnNk1/cDqVziyaUoBZhD3bs8Tzr7Hfg+UXUy+752oTAg6FC
+FD2BI4dhRwBThNLjxPZ1JllVio/6eZpD7LvtsciHqotjPnRGaZFk+Uus2w3IOcKQ
+UsfTTgGHkOW6qZRA9L7tyW7Gjg0ai7Pqw1Bv5P/0YaOlPeJ6BBBCjdkjEMYnxEVM
+GQ4F7ARZTFiIXpHKvSpmbWqJ6GV5g2HZn5DJbD3c89cwDQN2QoAsUMpzytmGsCMl
+wAvLFci8McibPzG91Yif6px6ocXEyETVgTfJxdqQgEedrf1FZVMY/7E5ZgsLQstn
+8aQH+AG5glT3sMfi1fBH0XXmtpGOXQ6uLTSrmifF0Rki5OzHuMSDsiYkt96LuWih
+9jjP1JSFE4rDwVEfCiDOj1V+KU4mE0Wa5ysxyBcf+Yv7W/cV2BUY0pgAVOX4XjTS
+PwEBwY8W7bqCrQV2Ij0BAEvDX22nb2wl4C23VD3E9P6vyVE/SES2/dgNVxii3cCz
+Ad5wPW58TPep9rx5f9+lBw==
+=bbs4
+-----END PGP MESSAGE-----
+m4=m3, t4 > t3
+-----BEGIN PGP MESSAGE-----
+
+hQIMA+cxB/nvl1l8AQ/+Mq2pRH6NzB6HfR5TOLIvZdQ7/Qe72jFkogy4/fLgpLUg
+uvGd56rHdgIn5KVD/UbYlGinHMVh3Zr9Y5u6ROpWgI7D0gWI6Ty5cK/XrZONluLQ
+o5Q0SUCVD/n0a5QufGDERdPhdSFWTZEPNxZ26SNk6/F8FfxH/WgdhECjWZd7638n
+siOwlv5ndLCLBBEi5mGQlf201mnkGcdYmQgmadEDH0o/s22URF8MXSENNT0Q7B5N
+TG02W9EMRYtEJcNmo+rvwbkFI5il+FJSx90nZRMXQM+bRYXc56cfRfpo7cBGGcXr
+67Yy9abZ1LI+7T2114xczyr3EDPnQB1JFDmsQQ3/RiQP2zM7mNxFNR8qPBwpLX32
+r7puT+SxeTBhY4HmwU8LZthDj26e0cr29q8zD06MUO5ANeYNlY0rX1MD9l76TCCr
+Av80wcIjcRaLJl1qrdthnBiFb/a2c8fyzFS5nA/NWHbOeuDMOeLGYZChQgU8BAIT
+5dEP+CTdGnLC91sz2ptiEC6SrypQ5r/vErkrzfJS7ZiPnHMU5zbPgsi7LiIqBTYt
+m1gFqzqN38jJMRTl3JvOuYAlPUQHlIV/b8W6sMVmzVj4SZ0RwlWyWzbo9ttYshJn
+YRYlehgfapUgyTzMuHhk6v607K27qgcORHPVzKRLhm1wdXx82gCnOVVURhNUCfjS
+PwG6HrfJn+LckSYcEufLIUfT5UXAHwt+nRuML2e/pFFOkSyl9P1iortDWGlwE7Y0
+IGHE/s1HS3NXZmqnZVVveQ==
+=jjtk
+-----END PGP MESSAGE-----
+
+m5=? t5>t4
+
+-----BEGIN PGP MESSAGE-----
+
+hQIMA+cxB/nvl1l8ARAArK/XtgLOVv23Exj31OHVdiw2PahEzZd2ipC9pGpFdMpq
+LDj6psBji6mjb9per5LzWeTAMoGLKeuis7jL/P46RUFgsqulL8+UBstEK+jgU/36
+g2u0iPxF027wssyHztfIBt6p1A8GrXc29szyf7qWFA43ljRHhfdGkILbOsPIN0qQ
+eqgAC+Wu18uQZMKB2eId3ieF+b3dMLvVofkC+EJxrjQv6rROd2fiTJ23FFvd/xG3
+wnkNbpgsNRc8RRomuh7lAAiPZMHQ8zWlcgelZDkPWYHlSOvKvg7AUMi87VfSV4eB
+eAtm9eKQrkyr8pExESzzHn3I+ysvsE3Ju8Ue5yYteMzDHy7qD/KIlWOQnhPEe9VQ
+b7MIHhSLi/XS9A+TTO3yssnP+PJT26cwSQh51d2qw8j034FyZDOm1xq6NIHJ/lnP
+wN6FvAn0uJ8dwKKUYMWORwi7GTyyrUDBzvDh9IeSdVcMlglWEP90vWQzvaCKSMuQ
+7+TfDf5cZwbkiiIxfzKE+y/LnU8Su748tstb0dBZL+eVR/Mo/rExCZEyXNFVBTDy
+BMYWcuzbyS/V/gzjYspe/c3bqSaQxG/d1M7QUGLMI1eNmwLFRO088Qac6fsOvw+A
+N1SBn3E6+vjtlcASCZXvyLwrXVowlmZCmeNEU09SELGcXMEiPzEfWFqFxT97Z7vS
+PgE5ae4KsM354oSbxPAT3LXBzCTNavfkfcQQAXrDrjpGKNGXxnD67zjIDCjpdwf9
+9abCBu5LYgOIfI7Dlg4F
+=ZeFs
+-----END PGP MESSAGE-----
+
+Question: given the above 4 pair (m,c) and (m(unkown), c):
+
+- Predict what is m
+Hints:
+ - What does PGP message structure contains (same prefix)
+ - Is public part of key always contained in PGP message
+ - "e" is the most occured letter
+ - we 
+"""
