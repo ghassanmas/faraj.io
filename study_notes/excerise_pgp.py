5509d8224a1ea006470f20a1d2f2035282a0d7e3
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2025-08-29 15:21:45 +0200

    research: somenotes relate/excerise relate to openpgp
     Relate #33

--- study_notes/excerise_pgp.py
+++ study_notes/excerise_pgp.py
@@ -103,3 +103,64 @@ Hints:
  - "e" is the most occured letter
  - we 
 """
+
+def playing_pgp():
+    # This take FFFF according to 3.5 RFC 9580 
+    # since  t1 = 28.7.25 ~~18:45 EST
+    ## 54 years (of which leap year)
+    # 13 of which are leap years
+    ### days = (41 * 365) + (13 * 366 ) + 31 + 28 + 31 + 30 + 31 + 30 
+    ### Hours = days * 24 + 17 (EST -1)
+    ### minutes = hours * 60 +( ~(45))
+    #### seconds = minutes * 60 +(~x) (repeat until 46?)
+    ##### Trannsfer to 'FFFF' (should relate to the session key)
+    # s = 1719769512 = with 12 seconds offect
+    time_elapsend_second_t1_1970 = 171976951200
+
+# It seems PGP in the above format is what is called armored (Is gpg/pgp way
+# to transfer bingary to ASCI code). 
+
+# The above all of types PGP message:, taking the first example 
+# converting it binary through the following commands: 
+# 1 - first to binary `gpg --dearmor message.asc` # by deaul the new file will be suffixed by gpg. 
+# 2 - Reading it in binary by hedxdump `hexdump -D message.asc.gpg`
+"""
+00000000  85 02 0c 03 e7 31 07 f9  ef 97 59 7c 01 0f fc 09  |.....1....Y|....| (85 is 1000 0101) (This means it's packet in legacy format) (it's type is 1 the 3th BIT) (the length of the body is two OCT first Bit)
+00000010  26 b9 ea 06 a9 4a 55 04  91 0c 99 35 01 71 d2 6e  |&....JU....5.q.n| (020C) is then the length the packet of which in digits is (524) Counting 524 OCT will lead to
+00000020  b3 f5 c8 4f 87 db ed ff  33 55 a1 d9 9c 5e 22 fb  |...O....3U...^".|
+00000030  17 d9 00 50 2d d9 56 3a  ee ac 56 c7 5f 15 b3 03  |...P-.V:..V._...|
+00000040  99 2f e5 83 5d 7d 87 21  ea ce b0 ab a7 c0 6f c2  |./..]}.!......o.|
+00000050  eb ce 21 5f 1c ec d1 39  2a 3a 6c 6f 5c ae 52 b9  |..!_...9*:lo\.R.|
+00000060  c8 df f6 6c 77 33 1a ed  38 c7 ad aa 5e dc 39 f1  |...lw3..8...^.9.|
+00000070  ca 23 62 67 d6 02 4c 2b  ab f6 e8 1e fd 0f ae c6  |.#bg..L+........|
+00000080  48 7e 40 0f 74 93 be d3  78 e5 7a a1 4f 1c 1b fd  |H~@.t...x.z.O...|
+00000090  99 f1 49 69 52 2f 85 3a  33 10 f4 6f 3e 18 11 00  |..IiR/.:3..o>...|
+000000a0  e3 77 64 ce e8 d7 c8 92  73 26 1e 55 71 34 82 e3  |.wd.....s&.Uq4..|
+000000b0  6c 1f 83 ad 0e 55 f4 de  79 08 9c c6 ab 5d 66 e6  |l....U..y....]f.|
+000000c0  23 60 63 4c a7 60 72 19  f3 f4 59 b2 45 0e 9f 6f  |#`cL.`r...Y.E..o|
+000000d0  e1 96 a9 5b c3 5a 8a 2d  d9 c0 1b 7d ec d7 8c 1f  |...[.Z.-...}....|
+000000e0  13 7b 57 38 c8 97 7d b5  29 36 b8 3f 2c 24 40 18  |.{W8..}.)6.?,$@.|
+000000f0  4b 5c 75 26 2e 01 54 da  fe f0 cf 4f 09 85 03 d0  |K\u&..T....O....|
+00000100  04 2a 1a aa 4b 06 b8 c6  82 ce e2 08 48 ae 89 ba  |.*..K.......H...|
+00000110  98 8a 34 51 6c 5b 8f 67  24 e3 5e 9a f1 5e bb ec  |..4Ql[.g$.^..^..|
+00000120  ed a7 0d a6 a7 cb fe 5e  ea 71 4f a8 52 26 3d 7a  |.......^.qO.R&=z|
+00000130  c6 c5 39 dd 02 26 c9 30  e7 cc ec 0a 63 77 c5 b2  |..9..&.0....cw..|
+00000140  42 0d 14 17 33 e7 6b c2  8e 6a 6d 7c 55 53 7d 89  |B...3.k..jm|US}.|
+00000150  82 4d f5 4f f5 85 27 b9  88 32 a8 62 b2 de 41 bd  |.M.O..'..2.b..A.|
+00000160  65 96 ef 75 b4 01 d6 5c  c1 d1 03 57 37 4d 8e aa  |e..u...\...W7M..|
+00000170  a3 6c 9a ce bf 79 15 84  e4 b1 ac b2 b3 50 07 36  |.l...y.......P.6|
+00000180  d0 b8 e6 67 05 bb d3 b4  df 5a f5 54 92 41 d1 53  |...g.....Z.T.A.S|
+00000190  96 ac e4 16 94 fb 23 c3  80 41 a4 08 d5 90 b3 d4  |......#..A......|
+000001a0  46 e0 00 60 5e fd 1e de  05 c8 81 bc 4d 8e 78 10  |F..`^.......M.x.|
+000001b0  03 02 4d 01 18 e3 dd 7b  ab 7a 5e 45 8c 55 05 ab  |..M....{.z^E.U..|
+000001c0  b1 61 4c af d4 9c 47 dd  17 e1 9c 0d e9 81 e4 42  |.aL...G........B|
+000001d0  ed 87 66 65 00 29 82 e2  e0 7a a4 90 cd f0 12 99  |..fe.)...z......|
+000001e0  60 4d bc bc f8 91 88 d1  a5 1c bc 5f 40 6b ce 71  |`M........._@k.q|
+000001f0  51 80 b9 73 86 d7 11 79  6c 23 da 43 3b 12 5b 3f  |Q..s...yl#.C;.[?|
+00000200  4f 48 32 79 be 7a 8f 62  60 2b 9c d9 4f 25 9d d2  |OH2y.z.b`+..O%..| (will lead to 9d). D2 translate to 1101 0010 Is a modern packet, and it's type 18 SEIPD
+00000210  40 01 fb e6 ff ba 45 49  2f b0 fb e1 43 5c 95 4f  |@.....EI/...C\.O| (The length is 40 of which  64)
+00000220  29 40 79 3a 4b 56 d1 4f  6b 7c e9 7b 3e 6d 91 d9  |)@y:KV.Ok|.{>m..|
+00000230  59 72 be a0 11 9e f6 95  90 b8 aa 2c 21 0d 3c 50  |Yr.........,!.<P|
+00000240  86 6b af 15 cf 6b ba 87  b6 89 3c 24 82 43 94 f6  |.k...k....<$.C..|
+00000250  02                                                |.|                 (Counting 64 will lead to here)
+"""
